615fadeafd16fd8864aecb8be45be540418df1ae
shivamu1818 <shivamuoadhyay4@gmail.com>
2026-04-11 16:50:30 +0530

    checkout page redirection done

--- src/pages/UnAuth/LoginForm.jsx
+++ src/pages/UnAuth/LoginForm.jsx
@@ -17,7 +17,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import useRedirection from "@/hooks/useRedirection";
-import { useAuth } from "@/context/AuthContext.jsx";
+import { useAuth } from "../../context/Authcontext";
 import api from "@/assets/api"
 
 export const loginSchema = z.object({
--- src/routes/GuestRoute.jsx
+++ src/routes/GuestRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "@/context/AuthContext.jsx";
+import { useAuth } from "../context/Authcontext";
 import { Spinner } from "@/components/ui/spinner";
 
 export default function GuestRoute() {

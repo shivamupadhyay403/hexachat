846fef631073d3b24a2e724a7f14006939c4552c
shivamu1818 <shivamuoadhyay4@gmail.com>
2026-04-11 17:04:44 +0530

    checkout page redirection done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const isProd = import.meta.env.VITE_APP_DEV === "production";
+
+createRoot(document.getElementById("root")).render(
+  !isProd ? (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  ) : (
     <App />
-  </StrictMode>,
-)
+  ),
+);

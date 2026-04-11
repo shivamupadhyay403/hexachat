d7266dfc8959d58b5f3ce9e0bf5f406c7c6865af
shivamu1818 <shivamuoadhyay4@gmail.com>
2026-04-11 16:48:49 +0530

    checkout page redirection done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "@/context/AuthContext";
+import { AuthProvider } from "./context/Authcontext";
 
 import GuestRoute from "./routes/GuestRoute";
 import ProtectedRoute from "./routes/ProtectedRoute";
